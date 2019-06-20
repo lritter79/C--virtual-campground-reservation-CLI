be5d60a10e19ffeadf1371dfa92195c343a043d9
Aaron Henteleff <aaronjayh@gmail.com>
2019-06-20 13:55:58 -0400

    Built out interfaces and park DAO

--- dotnet/Capstone/DAL/ICampgroundSqlDAO.cs
+++ dotnet/Capstone/DAL/ICampgroundSqlDAO.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Capstone.Models;
 
 namespace Capstone.DAL
 {
     public interface ICampgroundSqlDAO
     {
+        //returns a list of the Campgrounds from the DB
+        IList<Campground> GetCampgrounds();
     }
 }
--- dotnet/Capstone/DAL/ParkSqlDAO.cs
+++ dotnet/Capstone/DAL/ParkSqlDAO.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Text;
+using Capstone.Models;
 
 namespace Capstone.DAL
 {
-    public class ParkSqlDAO
+    public class ParkSqlDAO : IParkSqlDAO
     {
         private string ConnectionString;
 
@@ -14,7 +16,35 @@ namespace Capstone.DAL
 
         }
 
+        public IList<Park> GetParks()
+        {
+            List<Park> Parks = new List<Park>();
+
+            try
+            {
+                using(SqlConnection conn = new SqlConnection())
+                {
+                    conn.Open();
+
+                    SqlCommand cmd = new SqlCommand("SELECT * from park order by name;", conn);
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+
+                    }
 
 
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Error : " + ex.Message);
+                throw;
+            }
+
+            return Parks;
+        }
     }
 }
