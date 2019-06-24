1f809b895ca45c2bbdb5f104c77d45db4dd36acc
Aaron Henteleff <aaronjayh@gmail.com>
2019-06-24 09:50:49 -0400

    removed name from 30 day reservation display (privacy concerns)

--- dotnet/Capstone/DisplayHelper.cs
+++ dotnet/Capstone/DisplayHelper.cs
@@ -73,11 +73,11 @@ namespace Capstone
 
         public static void DisplayReservations(IList<Reservation> reservations)
         {
-            Console.WriteLine("Site_Number".PadRight(10) + "Confirmation #".PadRight(20) + "Reservation Name".PadRight(40) + "Start date".PadRight(15) + "End date".PadRight(15) + "Made on");
+            Console.WriteLine("Site_Number".PadRight(10) + "Confirmation #".PadRight(20) +  "Start date".PadRight(15) + "End date".PadRight(15) + "Made on");
 
             foreach(Reservation res in reservations)
             {
-                Console.WriteLine($"{res.Site_Id}".PadRight(10) + $"CGR{res.Reservation_Id}".PadRight(20) + $"{res.Name}".PadRight(40) + res.From_Date.ToShortDateString().PadRight(15) + res.To_Date.ToShortDateString().PadRight(15) + res.Create_Date.ToShortDateString());
+                Console.WriteLine($"{res.Site_Id}".PadRight(10) + $"CGR{res.Reservation_Id}".PadRight(20) +  res.From_Date.ToShortDateString().PadRight(15) + res.To_Date.ToShortDateString().PadRight(15) + res.Create_Date.ToShortDateString());
                
             }
             Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
